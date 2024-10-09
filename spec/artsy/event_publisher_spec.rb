5bb8ff4319dc02403264f2c6972c5e42fb590181
Joey Aghion <joey@aghion.com>
2024-10-09 18:12:41 -0400

    initial commit based on 'bundle gem ...' only

---
+++ spec/artsy/event_publisher_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Artsy::EventPublisher do
+  it "has a version number" do
+    expect(Artsy::EventPublisher::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
